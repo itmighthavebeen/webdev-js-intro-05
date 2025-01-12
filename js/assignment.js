d0ba8742a2beb9cb17b0f83695930512786b1fc2
Charlotte Ramsey <cramsey@aph.org>
2025-01-12 16:29:06 -0500

    initial commit of 05 project with arrays

--- js/assignment.js
+++ js/assignment.js
@@ -9,7 +9,8 @@ const todos = [
 ];
 
 // Declare your two immutable variables
-
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,46 +23,68 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    answerOneEl.textContent = todos.join(", "); //answer 1st question of display array   
 }
 
 function updateAnswerTwo() {
+    answerTwoEl.textContent = todos[2] || "No third item"; //answer 2nd question of what is the 3rd array value
+}
+function updateAnswerThree(){
+   // console.log("in 3");
+    removeLastValue();
 
+    console.log(popValue);
 }
 
 function removeLastValue() {
-
+    popValue = todos.pop();  // Removes the last value and stores it in popValue
+    
 }
 
 function removeFirstValue() {
-
+    shiftValue = todos.shift();  // Removes the first value and stores it in shiftValue
+    
 }
 
 function addShiftAndPopValues() {
-
+    todos.push(shiftValue);  // Adds the first removed item back to the start
+    todos.push(popValue);       // Adds the last removed item back to the end
+ 
 }
 
 function updateAnswerFour() {
-
+    console.log(todos,"in 4");
+    answerFourEl.textContent = todos.join(", "); 
 }
 
 function reverseTodoList() {
-
+    todos.reverse();  // Reverses the order of the items in the todos array
+   /* render();  */   
 }
 
 function updateAnswerFive() {
-
+    reverseTodoList();
+    answerFiveEl.textContent = todos.join(", ");
 }
-
 function updateAnswerSix() {
-
+    removeFirstValue();
+    addShiftAndPopValues();
+    answerSixEl.textContent = todos.join(", ");
 }
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    updateAnswerThree();
+    updateAnswerFour();
+    updateAnswerFive();
+    updateAnswerSix();
 }
 
-submissionBtn.addEventListener("click", function () {
+submissionBtn.addEventListener("click", function() {
     // Update this function
+   // console.log(todos);
+    
     render();
-});
+   // console.log(popValue);
+})
--- js/index.js
+++ js/index.js
@@ -1,3 +1,49 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+/*"use strict"
+let bachPieces = ["BWV 1: Wie schön leuchtet der Morgenstern (How Brightly Shines)", 
+    "BWV 2: Ach Gott, vom Himmel sieh darein ",
+    "BWV 3: Ach Gott, wie manches Herzeleid", 
+    "BWV 4: Christ lag in Todesbanden"];
+    
+    for (let i = 0; i < bachPieces.length; i++) {
+        console.log(bachPieces[i]);
+      }
+
+    
+
+bachPieces.forEach(item => {
+  console.log(item);
+});*/
+/*let bachPieces = [
+  "Toccata and Fugue in D minor", // String
+  12,                             // Number
+  true,                           // Boolean
+  { composer: "Johann Bach", year: 1727 }, // Object
+  [ "Allegro", "Largo", "Presto" ], // Array
+];
+
+console.log(bachPieces);
+bachPieces.forEach(item => {
+  console.log(typeof item);
+});
+bachPieces.pop(); 
+console.log(bachPieces);
+
+bachPieces.splice(2, 1); 
+console.log(bachPieces);
+// Add an item to the end of the array
+bachPieces.push("Air on the G String");
+console.log(bachPieces);
+
+// Add an item to the beginning of the array
+bachPieces.unshift(1727); 
+console.log(bachPieces);
+// Change the value of the element at index 0 (the first element, a string)
+bachPieces[0] = "Brandenburg Concerto No. 3";
+console.log(bachPieces); */
+
+
+
+
+
